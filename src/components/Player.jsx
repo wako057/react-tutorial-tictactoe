d9c25a0e384adeff54d8e61865f715f80cd40194
Gregory Gerard <gregory@flatchr.io>
2023-11-06 22:56:29 +0100

    feat: chapter 74/75 - Player component

---
+++ src/components/Player.jsx
@@ -0,0 +1,22 @@
+import {useState} from "react";
+
+export default function Player({name, symbol}) {
+    const [ isEditing, setIsEditing ] = useState(false);
+
+    console.log(`Player - isEditing: ${isEditing}`);
+    function handleEditClick() {
+        console.log(`handleIsEditing - isEditing: ${isEditing}`);
+        setIsEditing(!isEditing);
+    }
+
+    return (
+        <li>
+                <span className="player">
+                    { (!isEditing) ? <span className="player-name">{name}</span> : null}
+                    { (isEditing) ? <input type="text" required/> : null}
+                    <span className="player-symbol">{symbol}</span>
+                </span>
+            <button onClick={handleEditClick}>{ (!isEditing) ? 'Edit' : "Save"}</button>
+        </li>
+    );
+}
