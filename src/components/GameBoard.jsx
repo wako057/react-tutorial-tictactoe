b32e1a426fa1dae563cc4e1239302f96f5320f94
Gregory Gerard <gregory@flatchr.io>
2023-11-09 23:05:07 +0100

    feat: chapter 80/81 Lifting State Parent

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,22 +1,40 @@
+import {useState} from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+    const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex){
+        console.log(`rowIndex: ${rowIndex}   -   colIndex: ${colIndex}`)
+        setGameBoard((prevGameBoard) =>  {
+            const updatedBoard = [ ...prevGameBoard.map(innerArray => [...innerArray ])];
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+            return updatedBoard;
+        });
+
+        onSelectSquare();
+    };
+
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (<li key={rowIndex}>
-                <ol>
-                    {row.map((col, colIndex) => (
-                        <li key={colIndex}>
-                            <button>{col}</button>
-                        </li>
-                        )
-                    )}
-                </ol>
-            </li>)
+            { gameBoard.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                        { row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            </li>
+                            )
+                        )}
+                    </ol>
+                </li>
+                )
             )}
         </ol>
     )
