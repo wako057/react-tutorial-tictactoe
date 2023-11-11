94ea71183d1b70ed9b74967932f549b68a4ec726
Gregory Gerard <gregory@flatchr.io>
2023-11-11 23:24:04 +0100

    feat: chapter 88-89-90 Lifting computed value / deriving computed value from other computed value

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import {useState} from "react";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "../winning-combination";
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+];
 
 function derivedActivePlayer(gameTurns) {
     let currentPlayer = 'X';
@@ -15,6 +22,29 @@ function derivedActivePlayer(gameTurns) {
 function App() {
   const [ gameTurns, setGameTurns ] = useState([]);
   const activePlayer = derivedActivePlayer(gameTurns);
+  let gameBoard = initialGameBoard;
+  let winner;
+
+  for (const turn of gameTurns) {
+      const { square, player} = turn;
+      const { row, col } = square;
+      gameBoard[row][col] = player;
+  }
+
+  for (const combination of WINNING_COMBINATIONS ){
+      const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+      const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+      const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+      console.log(`firstSquareSymbol: ${firstSquareSymbol} - SecondSquareSymbol: ${secondSquareSymbol} - ThirdSquareSymbol: ${thirdSquareSymbol}`);
+      if (
+          firstSquareSymbol &&
+          firstSquareSymbol === secondSquareSymbol &&
+          firstSquareSymbol === thirdSquareSymbol
+      ) {
+          winner = firstSquareSymbol;
+      }
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
       setGameTurns(prevTurn => {
@@ -27,6 +57,7 @@ function App() {
 
           return updatedTurns;
       });
+
   }
 
   return (
@@ -44,9 +75,10 @@ function App() {
                   isActive={activePlayer === 'O'}
               />
           </ol>
+            {winner && <p>You won {winner} !</p>}
             <GameBoard
                 onSelectSquare={handleSelectSquare}
-                turns={gameTurns}
+                board={gameBoard}
             />
         </div>
         <Log turns={gameTurns}/>
